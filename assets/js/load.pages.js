a1935bc3e98a5e9ffdcc413dbf255482723823ab
martexte <afifkamarulimran@pm.me>
2022-11-23 18:10:06 +0800

    Updated

--- assets/js/load.pages.js
+++ assets/js/load.pages.js
@@ -2,10 +2,15 @@ $(function() {
 
     console.log( "ready!" );
 
+    // Static sidebar section
+    $("#header").load("static.html");
+
     // Intro section
     $("#top").load("intro.html");
 
     // Portfolio section
     $("#portfolio").load("portfolio.html");
+
+
 
 });
